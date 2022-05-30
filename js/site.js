0fbebf0736659c4ed29bcef0f88a10310fbdb5d1
johnwebdev32 <105568615+johnwebdev32@users.noreply.github.com>
2022-05-29 23:25:20 -0700

    Completed app portion
    App page and home page complete. Still need to finish the code page.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,38 @@
-//Call Hello World
-function helloWorld(){
-    alert("hello World");
+//Get string from the page
+
+function getValue(){
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
 }
+
+//Reverse the string
+
+function reverseString(userString){
+
+    let revString = [];
+
+    //reverse a string using a for loop
+
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index]; 
+    }
+    return revString;
+}
+
+//Display reversed string
+
+function displayString(revString){
+
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    document.getElementById("alert").classList.remove("invisible");
+}
+
+
+
